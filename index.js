797d20610a8ca67bd5a40fdcca209a9e6bcb1ff7
Fabien RIBES <fabien.ribes@eove.fr>
2017-11-21 10:12:06 +0100

    Use readline

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const fs = require('fs');
+const readline = require('readline');
 
-fs.readFile('country.csv', (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log('Read:' + data);
+const lineReader = readline.createInterface({
+  input: fs.createReadStream('country.csv')
+});
+
+const countries = [];
+
+lineReader.on('line', line => countries.push(line.split(',')));
+
+lineReader.on('close', () => {
+  fs.writeFile(
+    'country.json',
+    JSON.stringify(countries, null, 2),
+    'utf-8',
+    err => {
+      if (err) console.error(err);
+      else console.log('Done!');
+    }
+  );
 });
