fcfa99ced45e7e24c211904e8ffeb7964618513f
Fabien RIBES <fabien.ribes@eove.fr>
2017-11-21 11:04:59 +0100

    Done

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ const lineReader = readline.createInterface({
   input: fs.createReadStream('country.csv')
 });
 
-const countries = [];
+const arrayCountries = [];
 
-lineReader.on('line', line => countries.push(line.split(',')));
+lineReader.on('line', line => arrayCountries.push(line.split(',')));
 
-lineReader.on('close', () => {
+lineReader.on('close', toJson);
+
+function toJson() {
+  headers = arrayCountries.shift();
+  objCountries = arrayCountries.map(perCountry);
+
   fs.writeFile(
     'country.json',
-    JSON.stringify(countries, null, 2),
+    JSON.stringify(objCountries, null, 2),
     'utf-8',
     err => {
       if (err) console.error(err);
       else console.log('Done!');
     }
   );
-});
+
+  function perCountry(country) {
+    objCountry = headers.reduce((fields, field, index) => {
+      fields[field] = country[index];
+      return fields;
+    }, {});
+
+    return objCountry;
+  }
+}
